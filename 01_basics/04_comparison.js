fa63156b19bcc34a5458b92a11e59bc276840b95
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-09-20 08:14:38 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1)
+
+// two different data types should not be compare .
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// NOte : avoid this type of comparisons. 
+
+
+// === strictly check karta hai -> ye datatype bhi check karta hai
+
+
+console.log("2" == 2) // - true
+console.log("2" === 2) // - false
+
+console.log(1 == true);
+console.log(1 === true);
+
