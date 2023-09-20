ba93b08321d04f0c49522313c5673bff08d698d6
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-09-20 07:03:59 +0000

    Changes In variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,13 @@ const accountId = 144553
 let accountEmail = "raj@gmail.com"
 var accountPassword = "12345"
 accountCity = "Patna"
+let accountState;
+// accountId = 2
+accountEmail = "aditya@gmail.com"
+accountPassword = "56656"
+accountCity = "Bengaluru"
+console.log(accountId);
+/*
+Prefer not to use var because of issue in block scope and functional scope.
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
