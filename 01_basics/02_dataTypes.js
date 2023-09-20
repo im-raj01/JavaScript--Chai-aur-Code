af5789c0feb5ffeb4cbd7e189a40d8bb3605baca
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-09-20 07:29:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version of js.
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log("adi") // Code readability should be high
+console.log(25451+232323)
+
+let name = "adi"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => 0 || 1
+//null => standalone value
+// undefined => jab value define nhi kari hai but declare kar chuke ho
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
