0a56574354bec74e78a15074e77ce8bad00cfba4
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-09-20 07:47:00 +0000

    conversionOfDataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "adi"
+
+// const {score} = req.body//
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" -> 33
+//"33abc" = > NaN
+// true => 1
+
+let isLoggedIn = "vcyg"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(isLoggedIn);
+
+//1 - true ; 0- > false;
+// "" => false
+//"adit" => true;
+
+let Somenumber = 33
+let stringNumber = String(Somenumber)
+console.log(typeof stringNumber);
