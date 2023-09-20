4ce2af2a226f0b3072eb6b31e3e3b4c7c3c8867f
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-09-20 08:04:31 +0000

    conversionOpern

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,4 +23,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let Somenumber = 33
 let stringNumber = String(Somenumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations *********************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2*2/2);
+// console.log(2**3);
+
+let str1 =  "hello"
+let str2 = " adi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log("1" + (2 + 2));
+
+
+// console.log(1 + "2");
+// console.log(1 + 5 + "2");
+
+// console.log("1" + "2");
+
+// console.log(+true);
+// console.log(+"");
+// console.log(+false);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.table([num1,num2,num3])
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
