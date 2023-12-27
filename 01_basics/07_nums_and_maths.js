1d91a5925b93e151c5fdb9b158db159775d78d54
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-12-27 20:23:33 +0000

    nums and maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,40 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));
+
+const otherNumber = 123.896666;
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4,3,6,8));
+
+console.log(Math.random());
+// agar 1 se 10 tak chahiye to 
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min) 
+
+
+
+
