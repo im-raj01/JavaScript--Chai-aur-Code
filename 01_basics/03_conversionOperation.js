eb81ddbba7cdb31d768805d963099e0885535b4b
im-raj01 <133774718+im-raj01@users.noreply.github.com>
2023-12-27 11:48:46 +0000

    operations of data

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,13 +26,15 @@ let stringNumber = String(Somenumber)
 // console.log(typeof stringNumber);
 // console.log(Somenumber);
 
+
+
 // ***************** Operations *********************
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
 
-console.log(2+2*2/2);
-console.log(2**3);
+// console.log(2+2*2/2);
+// console.log(2**3);
 
 let str1 =  "hello"
 let str2 = " adi"
@@ -41,8 +43,8 @@ let str3 = str1 + str2
 // console.log(str3);
 
 // console.log("1" + 2);
-// console.log("1" + 2 + 2);
-// console.log("1" + (2 + 2));
+// console.log("1" + 2 + 2); // op--> 122
+// console.log("1" + (2 + 2)); // op --> 14
 
 
 // console.log(1 + "2");
@@ -50,7 +52,7 @@ let str3 = str1 + str2
 
 // console.log("1" + "2");
 
-// console.log(+true);
+// console.log(+true); // confusion code 
 // console.log(+"");
 // console.log(+false);
 
@@ -61,6 +63,7 @@ num1 = num2 = num3 = 2+2
 let gameCounter = 100;
 gameCounter++;
 ++gameCounter;
+console.log(gameCounter++);
 
 console.log(gameCounter);
 
